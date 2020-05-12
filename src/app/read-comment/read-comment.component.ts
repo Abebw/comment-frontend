374e72275d6076734cfb88b2dc366b163169e715
Abe Barth-Werb <abebarthwerb@gmail.com>
2020-05-12 17:10:55 -0400

    setup parameter to work

--- src/app/read-comment/read-comment.component.ts
+++ src/app/read-comment/read-comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-read-comment',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./read-comment.component.sass']
 })
 export class ReadCommentComponent implements OnInit {
-
+  @Input() comment;
   constructor() { }
 
   ngOnInit(): void {
